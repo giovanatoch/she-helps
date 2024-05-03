8d2996ab4a9fcf96bccb68a7cb686bc330b369cc
giovanatoch <tochtrop.giovana@gmail.com>
2024-05-02 21:37:00 -0300

    Criacao do projeto e add spring

---
+++ src/main/java/com/apishehelps/shehelps/SheHelpsApplication.java
@@ -0,0 +1,13 @@
+package com.apishehelps.shehelps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheHelpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheHelpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apishehelps/shehelps/SheHelpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apishehelps.shehelps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheHelpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
